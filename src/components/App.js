b781fc7626c459db898d5b294dcd6efcd2bd8a35
Georgi Ivanov <g.ivanov@blubito.com>
2019-03-06 09:49:06 +0200

    worng import for semantc ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 class App extends React.Component {
+    state = { language: 'english' };
+
+    onLanguageChange = () => {
+
+    }
+
     render() {
-        return <div> App!</div>
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="us flag" onClick={() => this.onLanguageChange('english')} />
+                    <i className="nl flag" onClick={() => this.onLanguageChange('dutch')} />
+                </div>
+            </div>
+        )
     }
 }
 
